de93efdb5cb17f66257e9b302c9fdf5a3d0bcf26
palsampear <zhgqhaha123@163.com>
2011-04-24 15:53:42 +0800

    q3

---
+++ hello.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+ void say_hi (void)
+int main(int argc, const char *argv[])
+{
+    say_hi();
+    return 0;
+}
+
+   void say_hi(void)
+   {
+       printf("hi peter\n");
+       }
+
+
+
+
+
+
+
+
+
+ 
+
+
+     
+
+
+              void say_hi(void)
+              {
+                  printf("hi peter\n");
+                  }
+                  ~                             
